648ddf9dbc2c90118bd7960a6159007125d2963e
Xusheng <xusheng@vector35.com>
2021-12-09 20:20:40 +0800

    Remove parameter rawViewName in BinaryNinja::FileMetadata::CreateSnapshotedView()

--- binaryninjaapi.h
+++ binaryninjaapi.h
@@ -983,8 +983,8 @@ namespace BinaryNinja {
 		bool Rebase(BinaryView* data, uint64_t address);
 		bool Rebase(BinaryView* data, uint64_t address,
 		    const std::function<bool(size_t progress, size_t total)>& progressCallback);
-		bool CreateSnapshotedView(BinaryView* data, const std::string& viewName, const std::string& rawViewName);
-		bool CreateSnapshotedView(BinaryView* data, const std::string& viewName, const std::string& rawViewName,
+		bool CreateSnapshotedView(BinaryView* data, const std::string& viewName);
+		bool CreateSnapshotedView(BinaryView* data, const std::string& viewName,
 								  const std::function<void(size_t progress, size_t total)>& progressCallback);
 
 		MergeResult MergeUserAnalysis(const std::string& name, const std::function<bool(size_t, size_t)>& progress,
